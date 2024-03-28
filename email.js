d8d5fd98790b6e49f78b831a96bca2ab44f21d0d
MaxLiebsch <liebsch@dipmaxexport.com>
2024-03-28 09:42:07 +0100

    feat: email service

---
+++ email.js
@@ -0,0 +1,31 @@
+import { createTransport } from "nodemailer";
+import "dotenv/config";
+import os from 'os'
+
+import { config } from "dotenv";
+
+config({
+  path: [`.env`],
+});
+
+const transporter = createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: true, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export const sendMail = async (mail) => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.SMTP_USER, // sender address
+      to: process.env.MAIL_RECIPIENT_1, // list of receivers
+      ...mail,
+    });
+  } catch (error) {
+    console.log("error:", error);
+  }
+};
