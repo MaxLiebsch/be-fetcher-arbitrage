5c31025d405b0ec6bc241774e4ed935bef763080
MaxLiebsch <liebsch@dipmaxexport.com>
2024-06-12 07:34:51 +0300

    fix: add timestamp

--- scheduler.pm2.config.cjs
+++ scheduler.pm2.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
       script: "yarn",
       args: "--cwd '/app' proxy",
       interpreter: "/bin/bash",
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         NODE_ENV: "production",
         PROXY_TYPE: "request",
@@ -19,6 +20,7 @@ module.exports = {
       script: "yarn",
       args: "--cwd '/app' scheduler:standalone",
       interpreter: "/bin/bash",
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       env: {
         NODE_ENV: "production",
         PROXY_TYPE: "request",
