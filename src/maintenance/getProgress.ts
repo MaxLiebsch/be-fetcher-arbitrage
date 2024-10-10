038eeec2a96c764deab056e275e37aaff6f05e49
MaxLiebsch <liebsch@dipmaxexport.com>
2024-10-10 10:44:34 +0200

    Update getProgress.ts

--- src/maintenance/getProgress.ts
+++ src/maintenance/getProgress.ts
@@ -1,3 +1,6 @@
+import { findPendingShops } from "../db/util/multiShopUtilities/findPendingShops.js";
+import { getTaskProgress } from "../db/util/multiShopUtilities/getTaskProgress.js";
+import { getShop } from "../db/util/shops.js";
 import {
   updateProgressDealsOnAznTasks,
   updateProgressDealsOnEbyTasks,
@@ -9,13 +12,24 @@ import {
 } from "../util/updateProgressInTasks.js";
 
 const main = async () => {
-  await updateProgressDealsOnAznTasks("mix");
-  await updateProgressDealsOnEbyTasks("mix");
-  await updateProgressNegDealTasks("mix");
-  await updateProgressInCrawlEanTask("mix");
-  await updateProgressInLookupCategoryTask();
-  await updateProgressInQueryEansOnEbyTask();
-  await updateProgressInLookupInfoTask();
+  // await updateProgressDealsOnAznTasks();
+  // await updateProgressDealsOnEbyTasks();
+  // await updateProgressNegDealTasks();
+  // await updateProgressInCrawlEanTask();
+  // await updateProgressInLookupCategoryTask();
+  // await updateProgressInQueryEansOnEbyTask();
+  // await updateProgressInLookupInfoTask();
+  const { pendingShops, shops } = await findPendingShops("LOOKUP_INFO");
+  console.log(
+    "pendingShops:",
+    pendingShops.map((s) => s.shop.d + ": " + s.pending)
+  );
+  // console.log('shops:', shops.map(s => s.d))
+  // console.log('pendingShops:', pendingShops)
+  // const shop = await getShop("digitalo.de");
+  // if (!shop) return
+  // const r = await getTaskProgress(shop.d, "LOOKUP_INFO", shop.hasEan);
+  // console.log('r:', r)
 };
 
 main().then((r) => process.exit(0));
