ff3856115ee4431e8449b1726d8244f54d32a0a4
MaxLiebsch <liebsch@dipmaxexport.com>
2024-10-22 10:55:44 +0200

    fix: test queries

--- __test__/mongo/testQueries.ts
+++ __test__/mongo/testQueries.ts
@@ -1,21 +1,117 @@
-import { countProductsPerCategoryAzn, countProductsPerCategoryEby} from "@dipmaxtech/clr-pkg";
-import { getArbispotterDb } from "../../src/db/mongo.js";
+import { getProductsCol } from "../../src/db/mongo.js";
+
+
 const testQueries = async () => {
-  const domain = "idealo.de";
-  const aggregation = countProductsPerCategoryAzn(domain);
+  const col = await getProductsCol();
 
-  const db = await getArbispotterDb();
-
-  console.log("aggregation:", aggregation);
-
-  const res = await db.collection(domain).aggregate(aggregation).toArray();
-  const reducedToObjectEby = res.reduce((acc, category) => {
-    const entry = Object.entries(category)[0];
-    return {
-      ...acc,
-      [entry[0]]: entry[1],
-    };
-  }, {})
+  const res = await col.aggregate([
+    {
+      $match: {
+        sdmn: ["idealo.de", "reichelt.de", "saturn.de"],
+        a_prc: { $gt: 0 },
+        costs: { $exists: true },
+        $or: [
+          { avg30_ahsprcs: { $exists: true, $gt: 0 } },
+          { avg30_ansprcs: { $exists: true, $gt: 0 } },
+          { avg90_ahsprcs: { $exists: true, $gt: 0 } },
+          { avg90_ansprcs: { $exists: true, $gt: 0 } },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        [`a_mrgn`]: {
+          $round: [
+            {
+              $subtract: [
+                {
+                  $ifNull: [
+                    "avg30_ahsprcs",
+                    {
+                      $ifNull: [
+                        "avg30_ansprcs",
+                        {
+                          $ifNull: ["avg90_ahsprcs", "avg90_ansprcs"],
+                        },
+                      ],
+                    },
+                  ],
+                },
+                {
+                  $add: [
+                    {
+                      $divide: [
+                        {
+                          $multiply: [
+                            "$a_prc",
+                            { $divide: ["$a_qty", "$qty"] },
+                          ],
+                        },
+                        {
+                          $add: [
+                            1,
+                            { $divide: [{ $ifNull: ["$tax", 19] }, 100] },
+                          ],
+                        },
+                      ],
+                    },
+                    {
+                      $subtract: [
+                        "$a_prc",
+                        {
+                          $divide: [
+                            "$a_prc",
+                            {
+                              $add: [
+                                1,
+                                { $divide: [{ $ifNull: ["$tax", 19] }, 100] },
+                              ],
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                    "$costs.tpt",
+                    "$costs.varc",
+                    "$costs.strg_2_hy",
+                    "$costs.azn",
+                  ],
+                },
+              ],
+            },
+            2,
+          ],
+        },
+      },
+    },
+    {
+      $addFields: {
+        [`a_mrgn_pct`]: {
+          $round: [
+            {
+              $multiply: [
+                {
+                  $divide: ["$a_mrgn", "$a_prc"],
+                },
+                100,
+              ],
+            },
+            2,
+          ],
+        },
+      },
+    },
+    {
+      $match: {
+        a_mrgn: { $gt: 0 },
+        a_mrgn_pct: { $gt: 0 },
+      },
+    },
+    {
+      $limit: 20,
+    },
+  ]);
+  console.log(res.toArray())
 };
 
 testQueries().then((r) => {
