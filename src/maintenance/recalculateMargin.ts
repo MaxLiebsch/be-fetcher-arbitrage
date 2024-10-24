b4b3f025df953670285cbea910506bcbde4ec060
MaxLiebsch <liebsch@dipmaxexport.com>
2024-10-24 17:22:48 +0200

    fix: migrate margin

---
+++ src/maintenance/recalculateMargin.ts
@@ -0,0 +1,32 @@
+import { getProductsCol } from '../db/mongo.js';
+import { recalculateAznMargin } from '../util/recalculateAznMargin.js';
+
+async function recalculateMargin() {
+  const col = await getProductsCol();
+
+  const products = await col
+    .find(
+      {
+        a_useCurrPrice: false,
+        a_mrgn: { $gt: 0 },
+        costs: { $exists: true },
+      },
+    )
+    .toArray();
+
+  for (let i = 0; i < products.length; i++) {
+    const spotterUpdate = {};
+    recalculateAznMargin(products[i], spotterUpdate);
+
+    const result = await col.updateOne(
+      { _id: products[i]._id },
+      { $set: { ...spotterUpdate } },
+    );
+    console.log('result: ',products[i]._id,'... ' ,result.modifiedCount);
+  }
+}
+
+recalculateMargin().then(() => {
+  console.log('done');
+  process.exit(0);
+});
